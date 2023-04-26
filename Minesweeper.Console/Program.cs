af438916f332544b6319438679721e24215f8b1c
Nahuel Rodriguez <nahuelrmaury@gmail.com>
2023-04-26 00:31:21 -0300

    first commit with first test cases

---
+++ Minesweeper.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using Minesweeper.Console;
+using Minesweeper.Core;
+using Minesweeper.Core.Enums;
+using Minesweeper.Core.Models;
+
+DifficultyLevel difficultyLevel = Printer.ChooseDifficultyLevel();
+GameSettings settings = DifficultyManager.GetGameSettingsByDifficultylevel(difficultyLevel);
+
+var field = FieldGenerator.GetRandomField(settings.Width, settings.Height, settings.Mines);
+
+var gameProcessor = new GameProcessor(field);
+
+var currentField = gameProcessor.GetCurrentField();
+
+Printer.PrintField(currentField);
+
+while (gameProcessor.GameState == GameState.Active)
+{
+    System.Drawing.Point coordinates = Printer.GetCoordinates();
+
+    gameProcessor.Open(coordinates.X, coordinates.Y);
+    currentField = gameProcessor.GetCurrentField();
+
+    Printer.PrintField(currentField);
+}
+
+Printer.PrintGameResult(gameProcessor.GameState);
